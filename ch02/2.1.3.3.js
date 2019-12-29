bbe51087647fd51514ab183e1e53ea5443e3b09f
hiroshimashu <hiroshimashu@gmail.com>
2019-12-30 00:16:11 +0900

    implement how to propagate err

---
+++ ch02/2.1.3.3.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+function readJSON(filename, callback) {
+	fs.readFile(filename, 'utf-8', (err, data) => {
+		let parsed;
+		if (err) return callback(err);
+
+		try {
+			parsed = JSON.parse(data);
+		} catch (err) {
+			return callback(err);
+		}
+		callback(null, parsed);
+	});
+}
