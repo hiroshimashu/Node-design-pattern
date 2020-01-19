bfdd6f0993be652720cda536a1372af0b829faa8
hiroshimashu <hiroshimashu@gmail.com>
2020-01-19 14:52:01 +0900

    implement implement unordered parallerl execution using stream

---
+++ ch05/index10.js
@@ -0,0 +1,34 @@
+import { Stream } from 'stream';
+
+class ParallelStream extends Stream.Transform {
+	constructor(userTransform) {
+		super({ objectNode: true });
+		this.userTransform = userTransform;
+		this.running = 0;
+		this.terminateCallback = null;
+	}
+
+	_transform(chunk, enc, done) {
+		this.running++;
+		this.userTransform(chunk, enc, this._onComplete.bind(this), this.push.bind(this));
+		done();
+	}
+
+	_flush(done) {
+		if (this.running > 0) {
+			this.terminateCallback = done;
+		} else {
+			done();
+		}
+	}
+
+	_onComplete(err) {
+		this.running--;
+		if (err) {
+			return this.emit('error', err);
+		}
+		if (this.running === 0) {
+			this.terminateCallback && this.terminateCallback();
+		}
+	}
+}
---
+++ ch05/index11.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const split = require('split');
+const request = require('request');
+const ParallelStream = require('./parallelStream');
+
+fs
+	.createReadStream(process.argv[2])
+	.pipe(split())
+	.pipe(
+		new ParallelStream((url, enc, done, push) => {
+			if (!url) return done();
+			request.head(url, (err, response) => {
+				push(url + 'is ' + (err ? 'down' : 'up') + '\n');
+				done();
+			});
+		})
+	)
+	.pipe(fs.createWriteStream('result.txt'))
+	.on('finish', () => console.log('All urls were checked'));
